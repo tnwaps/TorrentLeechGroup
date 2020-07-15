51e808fa445ed010b5b379a49e85e19e509858f7
Jᴏsʜᴜᴀ Cᴀʀʟᴛᴏɴ ✞ <56827413+JoshuaCarlton@users.noreply.github.com>
2020-07-15 21:25:59 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -242,6 +242,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 # msg += f"\nStatus : {file.status}"
                 msg += f"\nETA : {file.eta_string()}"
                 msg += f"\n<code>/cancel {gid}</code>"
+                msg += f"\n\n<b>@MC_LinkzZ</b>"
                 # LOGGER.info(msg)
                 if msg != previous_message:
                     await event.edit(msg)
