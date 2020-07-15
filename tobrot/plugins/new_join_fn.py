6c0df7809a5d554e21c93f1e865986c461c1b20f
Jᴏsʜᴜᴀ Cᴀʀʟᴛᴏɴ ✞ <56827413+JoshuaCarlton@users.noreply.github.com>
2020-07-16 01:42:09 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"Please Check the<a href='https://telegra.ph/MC-Leecher-Group-07-15'>Rules and Commands</a> Here..",
+        f"Please Check the <a href='https://telegra.ph/MC-Leecher-Group-07-15'>Rules and Commands</a> Here..",
         quote=True
     )
 
